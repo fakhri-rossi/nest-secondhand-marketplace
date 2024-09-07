beb8e1997ef06b9eb35a9828201d1efd887c7e00
Fakhri Rossi <rossifakhri@gmail.com>
2024-09-07 07:15:45 +0700

    Created Item Entity

---
+++ src/items/item.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  category: string;
+
+  @Column()
+  location: string;
+}
