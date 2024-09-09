b59c06a0c3622e13746ef2e933120c1c88469ab4
Fakhri Rossi <rossifakhri@gmail.com>
2024-09-09 16:50:42 +0700

    User Service: findAll and findOne

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,6 +9,14 @@ export class UsersService {
     @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
 
+  findAll() {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.usersRepository.findOneBy({ id });
+  }
+
   create(name: string, email: string, password: string) {
     const user = this.usersRepository.create({ name, email, password });
     return this.usersRepository.save(user);
